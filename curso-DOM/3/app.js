104ff9a3af3fa2849fc7b844844c4088b7bdfa9f
conrado <carlos_ag20@hotmail.com>
2025-03-31 16:31:35 -0300

    cambio: se agrego comentarios para entender mejor el codigo

--- curso-DOM/3/app.js
+++ curso-DOM/3/app.js
@@ -1,22 +1,24 @@
 const colores = ['rojo', 'verde', 'azul'];
 
 const valores = {};
-
+// Recorrer los colores
 colores.forEach((color) => {
   const input = document.getElementById(color);
   const texto = document.getElementById(`texto-${color}`);
-
+// Guardar los valores
   valores[color] = input.value;
+// Actualizar el texto  
   texto.textContent = input.value;
-
+// Actualizar el color
   input.addEventListener('change', (e) => {
     valores[color] = e.target.value;
     texto.textContent = valores[color];
     actualizarColor();
   });
 });
-
+//cambiar el color
 function actualizarColor() {
   const colorRGB = `rgb(${valores.rojo}, ${valores.verde}, ${valores.azul})`;
+  // Actualizar el color de fondo
   document.body.style.backgroundColor = colorRGB;
 }
